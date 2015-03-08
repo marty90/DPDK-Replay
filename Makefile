b4bf1787c19caaafa8e1b2471d0790ab84f040f7
root <root@sonda2.polito.it>
2015-03-08 18:42:27 +0100

    add sources

---
+++ Makefile
@@ -0,0 +1,26 @@
+ifeq ($(RTE_SDK),)
+$(error "Please define RTE_SDK environment variable")
+endif
+
+# Default target, can be overriden by command line or environment
+RTE_TARGET ?= x86_64-native-linuxapp-gcc
+
+include $(RTE_SDK)/mk/rte.vars.mk
+
+LDLIBS = -lpcap -lpthread -lm
+#LDFLAGS = -Wl,--rpath -Wl,/usr/local/lib
+
+# binary name
+APP = dpdk-replay
+
+# all source are stored in SRCS-y
+SRCS-y := src/main_replay.c
+
+CFLAGS += -O3
+#CFLAGS += -g
+CFLAGS += $(WERROR_FLAGS)
+
+include $(RTE_SDK)/mk/rte.extapp.mk
+
+
+
---
+++ src/main.h
@@ -0,0 +1,120 @@
+/* Includes */
+#define _GNU_SOURCE
+#include <pcap/pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <signal.h>
+#include <errno.h>
+#include <libgen.h>
+#include <sys/queue.h>
+#include <sys/syscall.h>
+#include <math.h>
+#include <sched.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include <rte_common.h>
+#include <rte_log.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_memzone.h>
+#include <rte_tailq.h>
+#include <rte_errno.h>
+#include <rte_eal.h>
+#include <rte_per_lcore.h>
+#include <rte_launch.h>
+#include <rte_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_interrupts.h>
+#include <rte_pci.h>
+#include <rte_debug.h>
+#include <rte_ether.h>
+#include <rte_ip.h>
+#include <rte_ethdev.h>
+#include <rte_ring.h>
+#include <rte_log.h>
+#include <rte_mempool.h>
+#include <rte_mbuf.h>
+#include <rte_string_fns.h>
+#include <rte_cycles.h>
+#include <rte_atomic.h>
+#include <rte_version.h>
+
+/* Useful macro for error handling */
+#define FATAL_ERROR(fmt, args...)       rte_exit(EXIT_FAILURE, fmt "\n", ##args)
+
+/* Function prototypes */
+static int main_loop_producer(__attribute__((unused)) void * arg);
+static void sig_handler(int signo);
+static void init_port(int i);
+static int parse_args(int argc, char **argv);
+void print_stats (void);
+void alarm_routine (__attribute__((unused)) int unused);
+int isPowerOfTwo (unsigned int x);
+
+/* RSS symmetrical 40 Byte seed, according to "Scalable TCP Session Monitoring with Symmetric Receive-side Scaling" (Shinae Woo, KyoungSoo Park from KAIST)  */
+uint8_t rss_seed [] = {	0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a,
+			0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a,
+			0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a,
+			0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a,
+			0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a, 0x6d, 0x5a
+};
+
+/* This seed is to load balance only respect source IP, according to me (Martino Trevisan, from nowhere particular) */
+uint8_t rss_seed_src_ip [] = { 	
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+/* This seed is to load balance only destination source IP, according to me (Martino Trevisan, from nowhere particular) */
+uint8_t rss_seed_dst_ip [] = { 	
+			0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+
+/* Struct for devices configuration for const defines see rte_ethdev.h */
+static const struct rte_eth_conf port_conf = {
+	.rxmode = {
+		.mq_mode = ETH_MQ_RX_RSS,  	/* Enable RSS */
+	},
+	.txmode = {
+		.mq_mode = ETH_MQ_TX_NONE,
+	},
+	.rx_adv_conf = {
+		.rss_conf = {
+			.rss_key = rss_seed,				/* Set the seed,					*/
+			.rss_key_len = 40,				/* and the seed length.					*/
+			.rss_hf = (ETH_RSS_IPV4_TCP | ETH_RSS_UDP) ,	/* Set the mask of protocols RSS will be applied to 	*/
+		}	
+	}
+};
+
+/* Struct for configuring each rx queue. These are default values */
+static const struct rte_eth_rxconf rx_conf = {
+	.rx_thresh = {
+		.pthresh = 8,   /* Ring prefetch threshold */
+		.hthresh = 8,   /* Ring host threshold */
+		.wthresh = 4,   /* Ring writeback threshold */
+	},
+	.rx_free_thresh = 32,    /* Immediately free RX descriptors */
+};
+
+/* Struct for configuring each tx queue. These are default values */
+static const struct rte_eth_txconf tx_conf = {
+	.tx_thresh = {
+		.pthresh = 36,  /* Ring prefetch threshold */
+		.hthresh = 0,   /* Ring host threshold */
+		.wthresh = 0,   /* Ring writeback threshold */
+	},
+	.tx_free_thresh = 0,    /* Use PMD default values */
+	.txq_flags = ETH_TXQ_FLAGS_NOOFFLOADS | ETH_TXQ_FLAGS_NOMULTSEGS,  /* IMPORTANT for vmxnet3, otherwise it won't work */
+	.tx_rs_thresh = 0,      /* Use PMD default values */
+};
+
